2d5fa7ef872e90ff36bd01739937af1634eab4f0
Igor Konoval <kip_y@sfa.org.ua>
2023-11-05 20:26:34 +0200

    add 1 stap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateClics = feedback => {
+    this.setState(prevState => ({
+      [feedback]: prevState[feedback] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <section>
+        <div>
+          <h2>Please leave feedback</h2>
+          <button onClick={() => this.updateClics('good')}>Good</button>
+          <button onClick={() => this.updateClics('neutral')}>Neutral</button>
+          <button onClick={() => this.updateClics('bad')}>Bad</button>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+        </div>
+      </section>
+    );
+  }
+}
